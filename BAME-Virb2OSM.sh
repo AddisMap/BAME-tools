600cd81c7e758c772f8bebfa347d235975d86dd9
Alexander Menk <alex.menk@gmail.com>
2014-11-27 11:45:21 +0300

    Configuration Facitility added

--- BAME-Virb2OSM.sh
+++ BAME-Virb2OSM.sh
@@ -11,77 +11,73 @@
 # Please adapt the following constants before the first use
 # The script is meant to be used by draging the GPX file on it (or supplying it as first parameter)
 # Please make sure you use the newest firmware
+#
+# Please add a file ".BAME-Virb2OSM.conf" based on the file delivered
 
-### constants to be adapted ###
+source ~/.BAME-Virb2OSM.conf
+
+while [ $# -ne 0 ]
+    do
+
+    gpxfile=$1
+
+    echo "############### Processing $1 ############## "
+    videofilerelative=`grep -Po '</name><link href="\K[^\"]*' "$1" | tr "\\\\" "\/"`
+    offset=`grep -Po '<gpxtrkoffx:StartOffsetSecs>(\K[^<]*)' "$1"`
+
+    # go three folders back
+    dn2=`dirname "$gpxfile"`
+
+    if [ "$dn2" == "." ]; then
+        dn='../..'
+    else
+        dn1=`dirname "$dn2"`
+        if [ "$dn1" == "." ]; then
+            dn='..'
+        else
+            dn=`dirname "$dn1"`
+        fi
+    fi
+
+    videofile=$dn$videofilerelative
+    base=`basename "$gpxfile" | sed "s/ /-/" | sed "s/_//" | sed "s/Track//" | sed "s/.gpx//"`
+    echo "Video file: $videofile"
+    echo "Offset: $offset"
+
+    target="$TARGETDIR"/$base
+    mkdir -p $target
+    echo "Target: $target"
+
+    echo Extracting frames with rate $RATE
+    #-ss 1
+    ffmpeg -v error -stats -i $videofile $RATE -ss 1 -y -an -q:v 0 -f image2 "$target/pic%05d.jpg"
+    # Get video date/time from exif info
+    echo Initially set time/date for all files at the beginning of the video file
+    exiftool -overwrite_original -CreateDate="`exiftool -TrackCreateDate -S $videofile | sed 's/.*: //' `" "$target"/pic*.jpg
+    # Adjust time of each jpeg
+    num=0 #num of seconds to add
+
+    echo "Setting time +1 second for each"
+    for file in "$target"/pic*.jpg
+    do
+     exiftool -overwrite_original -CreateDate+=0:00:$num $file > /dev/null # adjust file 1 +1s, file2 +2s, ..
+     num=$(( $num + 1 ))
+     echo -n "."
+     if [ $(( $num % 25 )) -eq 0 ]; then
+       echo $num;
+     fi
+    done
+
+    echo ""
+
+    echo "Geotagging"
+    # we might use
+    exiftool -overwrite_original -geosync=-$offset -geotag "$gpxfile" "-geotime<CreateDate" -P "$target"/pic*.jpg
 
 
-# Target dir for geocoded pictures
-# a subdirectory according to the gpx timestamp is automatically added
-TARGETDIR="/home/me/output/goes/here"
+    cp "$gpxfile" "$target"/"$base".gpx
 
-# set framerate (important)
-# for 30fps (NTSC) set to 1 - so every one second a frame is extracted
-# for timelapse - once per second, please set to 30 so every frame is extracted
+    echo "done"
 
-#RATE=""     # time lapse
-RATE="-r 1"  # 30 fps
-
-
-gpxfile=$1
-
-videofilerelative=`grep -Po '</name><link href="\K[^\"]*' "$1" | tr "\\\\" "\/"`
-offset=`grep -Po '<gpxtrkoffx:StartOffsetSecs>(\K[^<]*)' "$1"`
-
-# go three folders back
-dn2=`dirname "$gpxfile"`
-
-if [ "$dn2" == "." ]; then
-  dn='../..'
-else
-  dn1=`dirname "$dn2"`
-  if [ "$dn1" == "." ]; then
-    dn='..'
-  else
-    dn=`dirname "$dn1"`
-  fi
-fi
-
-videofile=$dn$videofilerelative
-base=`basename "$gpxfile" | sed "s/ /-/" | sed "s/_//" | sed "s/Track//" | sed "s/.gpx//"`
-echo "Video file: $videofile"
-echo "Offset: $offset"
-
-target="$TARGETDIR"/$base
-mkdir -p $target
-echo "Target: $target"
-
-echo Extracting frames at $RATE Hz rate
-#-ss 1
-ffmpeg -i $videofile $RATE -ss 1 -y -an -q:v 0 -f image2 "$target/pic%05d.jpg"
-# Get video date/time from exif info
-echo Initially set time/date for all files at the beginning of the video file
-exiftool -overwrite_original -CreateDate="`exiftool -TrackCreateDate -S $videofile | sed 's/.*: //' `" "$target"/pic*.jpg
-# Adjust time of each jpeg
-num=0 #num of seconds to add
-
-echo "Setting time +1 second for each"
-for file in "$target"/pic*.jpg
-do
- exiftool -overwrite_original -CreateDate+=0:00:$num $file > /dev/null # adjust file 1 +1s, file2 +2s, ..
- num=$(( $num + 1 ))
- echo -n "."
- if [ $(( $num % 25 )) -eq 0 ]; then
-   echo $num;
- fi
+    shift
 done
-
-echo ""
-
-echo "Geotagging"
-# we might use
-exiftool -overwrite_original -geosync=-$offset -geotag "$gpxfile" "-geotime<CreateDate" -P "$target"/pic*.jpg
-
-
-cp "$gpxfile" "$target"/"$base".gpx
-
-echo "done"
